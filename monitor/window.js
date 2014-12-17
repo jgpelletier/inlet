8c4106aa4e60e4440fff31decc8324faf5668995
Joshua Pelletier <jgpelletier@gmail.com>
2014-12-17 14:44:38 -0800

    Unlink tail.

--- monitor/window.js
+++ monitor/window.js
@@ -2,7 +2,7 @@
 function Window () {
     this.count = 0
     this.sum = 0
-    this.head = {}
+    this.head = { head: true, value: null }
     this.head.next = this.head.previous = this.head
 }
 
@@ -11,7 +11,7 @@ Window.prototype.sample = function (value) {
     this.count++
     this.sum += value
     var node = {
-        when: Date.now,
+        when: Date.now(),
         value: value,
         next: this.head.next,
         previous: this.head
@@ -23,6 +23,10 @@ Window.prototype.sample = function (value) {
 Window.prototype.__defineGetter__('stats', function () {
     // purge expired values, calculate average.
     // node === this.head
+
+    this.head.previous = this.head.previous.previous
+    this.head.previous.next = this.head.next
+
     return { average: 0 }
 })
 
