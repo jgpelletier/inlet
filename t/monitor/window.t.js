490d18e1d98d9b0b91ebf201d01cee1d22a00426
Joshua Pelletier <jgpelletier@gmail.com>
2014-12-18 06:49:10 -0800

    Assert a count of 0 returns null.

--- t/monitor/window.t.js
+++ t/monitor/window.t.js
@@ -1,9 +1,13 @@
 
-require('../proof')(1, prove)
+require('../proof')(2, prove)
 
 function prove (assert) {
     var Window = require('../../monitor/window')
     var metric = new Window(60000)
+    //setTimeout(metric.sample(1), 6000)
+    //setTimeout(metric.sample(2), 15000)
+    //setTimeout(metric.sample(3), 25000)
+    assert(metric.stats,  null , 'stats')
     metric.sample(1)
     metric.sample(2)
     metric.sample(3)
